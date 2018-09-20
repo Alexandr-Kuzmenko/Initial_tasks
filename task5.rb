63ccb64871e8c1689c96fbf6c8b6d61dba834d3d
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-09-20 15:15:04 +0300

    optimized

--- task5.rb
+++ task5.rb
@@ -1,18 +1,18 @@
 # Написать метод, который принимает строку и приводит её в CamelCase, 
 # ruby_case_underscore и css-case.
 class Task05  
-  def formatize(format, text)   
-    text = text.gsub(' ', '') if format == 1
-    text = text.gsub(' ', '_') if format == 2
-    text = text.gsub(' ', '-') if format == 3
-    return text
+  def formatize(text, format)
+    #puts text.split(' ').map(&:capitalize).join(' ') if format == 1
+    puts text.split(' ').map(&:capitalize).join if format == 1
+    puts text.downcase.gsub(' ', '_') if format == 2
+    puts text.gsub(' ', '-') if format == 3   
   end
 
   def bodytask	
     str = 'i love ruby'
-    puts "Выберите тип преобразования:\n1: camel\n2: underscope\n3: css"
+    puts "Choose one from list to change the string:\n1 :camel\n2 :underscope\n3 :css"
     format = gets.chomp.to_i
-    puts formatize(format, str)
+    puts formatize(str, format)
   end
 end
-Task05.new.bodytask
+# Task05.new.bodytask
