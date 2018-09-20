2c3f72f0f3aff883444e51536a65230087f98edb
Alexandr Kuzmenko <Alexandr.Kuzmenko@faceit-team.com>
2018-09-20 11:46:12 +0300

    made shorter

--- task1.rb
+++ task1.rb
@@ -1,18 +1,10 @@
 # Вывести в консоль, 30 раз "<3 ruby", каждый в новой строке, 
 # 10ый, 20ый, и 25ый вывод вывести просто "ruby".
 class Task01
-  def owntask
-    i = 1
-    str1 = '<3 ruby'
-    str2 = 'ruby'
-    while i <= 30 do
-      if i == 10 || i == 20 || i == 25
-        puts "#{i}  #{str1}"
-      else
-        puts "#{i}  #{str2}"
-      end
-      i += 1
+  def bodytask
+    30.times do |i|
+      [10, 20, 25].include?(i+1) ? puts("#{i + 1} <3 ruby") : puts("#{i + 1} ruby")
     end
   end
 end
-Task01.new.owntask
+Task01.new.bodytask
